3453d3ca18440745eb3f5c69813d7a1704615957
venkat.kamani <venkat16042020@gmail.com>
2026-06-28 16:16:47 +0530

    10

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [{
+    path: '',
+    pathMatch: 'full',
+    loadComponent: () => 
+    {
+        return import('./home/home').then((m) => m.Home);
+    },
+},
+{
+    path: 'greeting',
+    loadComponent: () => 
+        {return import('./components/greeting/greeting').then((m) => m.Greeting);
+        }
+}
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -2,9 +2,10 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Home } from './home/home';
 import { Header } from './components/header/header';
+import { Greeting } from './components/greeting/greeting';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Home],
+  imports: [RouterOutlet, Header],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
