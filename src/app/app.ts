e95a655066c724318ece1100422a42be4f23f796
venkat.kamani <venkat16042020@gmail.com>
2026-06-28 13:52:49 +0530

    4

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { Home } from './home/home';
+import { Header } from './components/header/header';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header, Home],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('Welcome to Apps for Good Life');
+  protected readonly title1 = 'Good start for your life';
 }
