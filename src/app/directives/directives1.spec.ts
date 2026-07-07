860fc7f0652314115f400caf756e405e577e7976
venkat.kamani <venkat16042020@gmail.com>
2026-07-07 14:46:37 +0530

    19

---
+++ src/app/directives/directives1.spec.ts
@@ -0,0 +1,8 @@
+import { Directives1 } from './directives1';
+
+describe('Directives1', () => {
+  it('should create an instance', () => {
+    const directive = new Directives1();
+    expect(directive).toBeTruthy();
+  });
+});
---
+++ src/app/directives/directives1.ts
@@ -0,0 +1,9 @@
+import { Directive } from '@angular/core';
+
+@Directive({
+  selector: '[appDirectives1]',
+})
+export class Directives1 {
+  constructor() {
+  }
+}
